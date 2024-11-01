295b8f705e7f603839f2032ca3c6c1b7b24730c2
quangdaon <me@quangdao.com>
2024-10-31 19:45:10 -0500

    Lowercase urls

--- src/MtgBans.Api/Program.cs
+++ src/MtgBans.Api/Program.cs
@@ -8,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddRouting(options =>
+{
+  options.LowercaseUrls = true;
+  options.LowercaseQueryStrings = true;
+});
+
 builder.Services.AddMtgBans(builder.Configuration);
 
 var app = builder.Build();

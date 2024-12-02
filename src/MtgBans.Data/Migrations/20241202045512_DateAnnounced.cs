ae7c114440298a1e20144be2883e9274692387f9
quangdaon <me@quangdao.com>
2024-12-01 22:58:37 -0600

    Add date announced

---
+++ src/MtgBans.Data/Migrations/20241202045512_DateAnnounced.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MtgBans.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DateAnnounced : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "date_announced",
+                table: "announcements",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+            
+            // Copy values from the date_effective column to the date_announced column
+            migrationBuilder.Sql("UPDATE announcements SET date_announced = date_effective");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "date_announced",
+                table: "announcements");
+        }
+    }
+}

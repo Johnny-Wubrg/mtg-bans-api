22dece204f49f30aad19d450fa474539ab17e1ac
Quangdao Nguyen <me@quangdao.com>
2024-12-30 19:09:06 -0600

    Add Effective Timeline to Classifications (#13)
    * Classification Timelines
    
    * Prefer first
    
    * Classification Sorting

--- src/MtgBans.Data/Entities/Classification.cs
+++ src/MtgBans.Data/Entities/Classification.cs
@@ -6,7 +6,16 @@ public class Classification
 {
   [Key]
   public int Id { get; set; }
+  
+  public int DisplayOrder { get; set; }
 
   [Required, MaxLength(200)]
   public string Summary { get; set; }
+  
+  [Required]
+  public DateOnly DateApplied { get; set; }
+
+  public DateOnly? DateLifted { get; set; }
+  
+  public ICollection<Card> Cards { get; set; }
 }
---
+++ src/MtgBans.Data/Migrations/20241230183132_ClassificationSorting.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MtgBans.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClassificationSorting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "display_order",
+                table: "classification",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+            
+            migrationBuilder.Sql("UPDATE classification SET display_order = id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "display_order",
+                table: "classification");
+        }
+    }
+}

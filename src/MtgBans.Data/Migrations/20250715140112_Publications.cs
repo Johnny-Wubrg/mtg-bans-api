ade50eea6bf84cf26985aaee05097a971ad380bd
Quangdao Nguyen <me@quangdao.com>
2025-07-15 12:12:10 -0500

    Split publications change to allow prep

--- src/MtgBans.Data/Migrations/20250715140112_Publications.cs
+++ src/MtgBans.Data/Migrations/20250715140112_Publications.cs
@@ -94,40 +94,11 @@ namespace MtgBans.Data.Migrations
                 JOIN publications p ON p.uri = source_uri
                 WHERE a.sources IS NOT NULL;
             ");
-
-            // Drop the old sources column
-            migrationBuilder.DropColumn(
-                name: "sources",
-                table: "announcements");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            // Add back the sources column
-            migrationBuilder.AddColumn<string[]>(
-                name: "sources",
-                table: "announcements",
-                type: "text[]",
-                nullable: true);
-
-            // Migrate data back from publications to sources array
-            migrationBuilder.Sql(@"
-                -- Populate sources array from publications relationships
-                UPDATE announcements 
-                SET sources = subquery.source_array
-                FROM (
-                    SELECT 
-                        a.id,
-                        array_agg(p.uri) as source_array
-                    FROM announcements a
-                    JOIN announcement_publication ap ON a.id = ap.announcements_id
-                    JOIN publications p ON ap.sources_id = p.id
-                    GROUP BY a.id
-                ) subquery
-                WHERE announcements.id = subquery.id;
-            ");
-
             // Drop the new tables
             migrationBuilder.DropTable(
                 name: "announcement_publication");

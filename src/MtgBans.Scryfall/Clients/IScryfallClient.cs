769dc3a2fe9c2439c11572f59de314cae49e5870
quangdaon <me@quangdao.com>
2026-09-16 21:20:39 -0500

    Remove publisher endpoints

--- src/MtgBans.Scryfall/Clients/IScryfallClient.cs
+++ src/MtgBans.Scryfall/Clients/IScryfallClient.cs
@@ -5,15 +5,6 @@ namespace MtgBans.Scryfall.Clients;
 
 public interface IScryfallClient
 {
-  [Get("/cards/search?q=!%22{cardName}%22&order=released&dir=asc&unique=prints&include_extras=true")]
-  Task<ScryfallDataset<ScryfallCard>> GetCardByName(string cardName, CancellationToken cancellationToken = default);
-
-  [Get("/cards/search?q=oracleid:{oracleId}&order=released&dir=asc&unique=prints&include_extras=true")]
-  Task<ScryfallDataset<ScryfallCard>> GetCardByOracleId(Guid oracleId, CancellationToken cancellationToken = default);
-
   [Get("/cards/search?q={query}&unique=cards&order=name")]
   Task<ScryfallDataset<ScryfallCard>> Search(string query, CancellationToken cancellationToken = default);
-
-  [Get("/sets")]
-  Task<ScryfallDataset<ScryfallSet>> GetSets(CancellationToken cancellationToken = default);
 }

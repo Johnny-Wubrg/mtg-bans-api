81d98d0cf5ecbf42d9e8ede2852bde4025ea3507
Quangdao Nguyen <me@quangdao.com>
2026-09-21 11:45:40 -0500

    Make Next Date Nullable (#34)

---
+++ src/MtgBans.Data/Migrations/20260921163944_MakeAnnouncementDateNextProjectedNullable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MtgBans.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeAnnouncementDateNextProjectedNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "date_next_projected",
+                table: "announcements",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.Sql(
+                "UPDATE announcements SET date_next_projected = NULL WHERE date_next_projected = '0001-01-01';");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "UPDATE announcements SET date_next_projected = '0001-01-01' WHERE date_next_projected IS NULL;");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "date_next_projected",
+                table: "announcements",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1),
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+        }
+    }
+}
